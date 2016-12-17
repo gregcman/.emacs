4df5d1e49b0ac528e41b66c930fa3d67860e16bb
terminal625 <zpersoncool@gmail.com>
2016-12-17 16:41:39 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,16 +1,15 @@
-(setq inferior-lisp-program "sbcl")
-
-;; Tell emacs where is your personal elisp lib dir
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
+  ;; Replace "sbcl" with the path to your implementation
+  (if t 
+  	(setq inferior-lisp-program "/usr/local/bin/sbcl")
+  	(setq inferior-lisp-program "/usr/local/bin/ccl"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 103 :width normal)))))
+ )
 
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
@@ -21,17 +20,20 @@
 (package-initialize) ;; You might already have this line
 
 (show-paren-mode 1)
+(add-to-list 'load-path "~/.emacs.d/emacs plugins/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 ;(require 'rainbow-delimiters)
 ;(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 ;(add-hook 'slime-repl-mode-hook 'rainbow-delimiters-mode)
 
 ;(require 'rainbow-mode)
-;(add-hook 'progmode-hook 'rainbow-mode)
+;(add-hook 'pro-mode-hook 'rainbow-mode)
 ;(add-hook 'lisp-mode-hook 'rainbow-mode)
 
 (global-set-key (kbd "C-M-f") 'sexp-move-forward)  ; Ctrl+Alt+f
 (global-set-key (kbd "C-M-b") 'sexp-move-backward) ; Ctrl+Alt+b
+
 
 ;;;;;;;;;;;;;
 ;;;;;;;;;
@@ -45,21 +47,20 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+;(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
 
 ;;autocomplete basic configuration
 (ac-config-default)
 
 (desktop-save-mode 1)
 
-(require 'i3-integration)
-(i3-one-window-per-frame-mode-on)
-
 ;;slime autocomplete mode
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
+
+(require 'visual-indentation-mode)
 
 ;; Stop SLIME's REPL from grabbing DEL,
 ;; which is annoying when backspacing over a '('
@@ -116,7 +117,7 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "6c62b1cd715d26eb5aa53843ed9a54fc2b0d7c5e0f5118d4efafa13d7715c56e" "f3d6a49e3f4491373028eda655231ec371d79d6d2a628f08d5aa38739340540b" default)))
  '(fci-rule-character-color "#202020")
  '(fci-rule-color "#3E3D31")
- '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(fringe-mode 0 nil (fringe))
  '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
  '(highlight-symbol-colors
    (--map
@@ -140,16 +141,7 @@
  '(hl-fg-colors
    (quote
     ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
- '(inhibit-startup-screen nil)
- '(initial-buffer-choice t)
- '(initial-scratch-message
-   ";; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
-;; Hello from the emacs side
-;; I mustve meta'd a thousand times
-
-")
+ '(inhibit-startup-screen t)
  '(magit-diff-use-overlays nil)
  '(main-line-color1 "#1E1E1E")
  '(main-line-color2 "#111111")
@@ -169,7 +161,6 @@
  '(term-default-bg-color "#fdf6e3")
  '(term-default-fg-color "#657b83")
  '(tool-bar-mode nil)
- '(tooltip-mode nil)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
@@ -198,4 +189,3 @@
    ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
  '(xterm-color-names-bright
    ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
-(put 'narrow-to-region 'disabled nil)
